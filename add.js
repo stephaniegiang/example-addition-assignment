eb43736fe25299448160c379d1a9da7c86a4f26a
Stephanie Giang <stephaniegiang@users.noreply.github.com>
2019-09-26 18:41:25 -0400

    Update add.js

--- add.js
+++ add.js
@@ -2,6 +2,9 @@ module.exports = (left, right) => {
   // Write a function that adds the left number to the right number
   // and returns it. If either of the inputs are not valid numbers
   // throw an Error
+  if ((typeof left == "number") || (typeof left == "number")){
+    throw Error("One of the inputs is not a number")
+  }
   result = left + right
   return result 
 }
